e571d7f4b58a86867c47b1af77f96e40a80bf6ec
Oleksiy Rudenko <8686631+OleksiyRudenko@users.noreply.github.com>
2024-09-13 20:34:08 +0200

    Add data store: challenges config handling

---
+++ src/data-store/challenges.js
@@ -0,0 +1,34 @@
+import {challengesConfig} from "./config-challenges";
+
+/**
+ * Returns config of challenges that applicable to current date, only computations applicable
+ * @param {Array[Object]} challengesConfig
+ * @param {Date} date
+ * @param {Boolean} isRaw - if challenges config is raw source
+ */
+export const getChallengesByDate = (challengesConfig, date, isRaw = true) => {
+  challengesConfig = isRaw ? prepareChallengesConfig(challengesConfig) : challengesConfig;
+
+}
+
+/**
+ * Makes basic transformations in challenges config copy.
+ * 1. dateStrings => Date objects
+ * @param {Array[Object]} challengesConfig
+ * @returns {Array[Object]}
+ */
+export const prepareChallengesConfig = challengesConfig =>
+  JSON.parse(JSON.stringify(challengesConfig))
+    .forEach(challenge => {
+      challenge.phases.forEach(phase => {
+        if (typeof (phase.timeframe) !== "undefined") {
+          phase.timeframe.from = new Date(phase.timeframe.from);
+          phase.timeframe.till = new Date(phase.timeframe.till);
+        }
+      })
+    })
+
+const getChallengesByDate = (challengesConfig, date) =>
+  challengesConfig.filter(
+    challenge => challenge.phases.some(phase => true)
+  )
---
+++ src/data-store/config-challenges.js
@@ -0,0 +1,238 @@
+export const challengesConfig = [
+  {
+    name: "grarowerowa 2024 Gdansk",
+    description: "",
+    docs: {
+      regulations: "",
+      computations: "https://contests.v3.activy.pl/pages/pl/30b42c7a-7df5-46fa-9a67-c3883f896a6f/rules",
+      rankBoard: "",
+    },
+    sports: [
+      {
+        groupName: "wheels",
+        // https://support.strava.com/hc/en-us/articles/216919407-Supported-Sport-Types-on-Strava
+        activities: ["Ride", "Inline Skate", "Mountain Bike Ride", "Road bike", "E-Bike Ride", "E-Mountain Bike Ride",
+          "Gravel Ride", "Handcycle", "Skateboard"],
+      },
+    ],
+    activityDayMapping: "activityEnd",
+    phases: [
+      {
+        id: "default",
+        metrics: [
+          {
+            id: "pts",
+            frequency: "onActivityEnd",
+            inputs: [
+              {
+                id: "mileage",
+                source: "API",
+                dataProvider: "Strava",
+                sportGroups: ["wheels",],
+              },
+            ],
+            outputs: [
+              {
+                id: "pts",
+                name: "pts per activity",
+                description: "Pts per activity",
+                computationRules: [
+                  {
+                    op: "floor",
+                    params: [
+                      {
+                        source: "inputs", id: "mileage",
+                      }
+                    ]
+                  },
+                ],
+              }
+            ],
+          },
+          {
+            name: "globalRank",
+            frequency: "cumulative",
+            inputs: [
+              {
+                id: "rank", // .name = .id if not specified
+                source: "user",
+              },
+              {
+                id: "totalParticipants",
+                name: "total participants count",
+                source: "user",
+              },
+            ],
+            outputs: [
+              {
+                id: "topX",
+                name: "better than X%",
+                description: "Track your ranking progress among entire participants population. Fixed at the end of each day.",
+                computationRules: [
+                  {
+                    op: "div",
+                    params: [
+                      {source: "inputs", id: "rank",},
+                      {source: "inputs", id: "totalParticipants",},
+                    ],
+                  },
+                ],
+              },
+            ],
+          }
+        ]
+      },
+      {
+        id: "x1",
+        name: "September",
+        description: "In September pts are equivalent to kilometers rounded down to integer",
+        timeframe: {from: "2024-09-01T00:00:00+2:00", till: "2024-09-30T23:59:59+2:00",},
+        import: ["default"],
+      },
+      {
+        id: "x2",
+        name: "October x2",
+        description: "In October pts are x2!",
+        timeframe: {from: "2024-10-01T00:00:00+2:00", till: "2024-10-31T23:59:59+2:00",},
+        import: ["default"],
+        metrics: [
+          {
+            id: "pts", // amends metric from defaults entry
+            outputs: {
+              id: "pts",
+              computationRulesAmendmentMode: "append", // as opposed to rewrite
+              computationRules: [
+                {
+                  op: "mult",
+                  params: [2],
+                },
+              ],
+            },
+          },
+        ],
+      },
+      {
+        id: "x3",
+        name: "November x3",
+        description: "In November pts are x3!",
+        timeframe: {from: "2024-11-01T00:00:00+2:00", till: "2024-11-30T23:59:59+2:00",},
+        import: ["default"],
+        metrics: [
+          {
+            id: "pts", // amends metric from defaults entry
+            outputs: {
+              id: "pts",
+              computationRulesAmendmentMode: "append", // as opposed to rewrite
+              computationRules: [
+                {
+                  op: "mult",
+                  params: [3],
+                },
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "ringSportsClub2024-Autumn",
+    name: "Ring Sport Club 2024 Autumn",
+    description: "",
+    timeframe: {from: "2024-09-01T00:00:00+2:00", till: "2024-10-31T23:59:59+2:00",},
+    docs: {
+      regulations: "",
+      computations: "",
+      rankBoard: "https://docs.google.com/spreadsheets/d/1UyYF9ajtDgZQbGOysNaaghUO_ERdgURJhMMwP2ayRu0/edit?gid=1391662661#gid=1391662661",
+    },
+    sports: [
+      {
+        groupName: "Cycling",
+        activities: ["Ride", "Mountain Bike Ride", "Gravel Ride",],
+      },
+      {
+        groupName: "Running",
+        activities: ["Run", "Trail Run",],
+      },
+      {
+        groupName: "Walking",
+        activities: ["Walk", "Hike",],
+      },
+      {
+        groupName: "Swimming",
+        activities: ["Swim",],
+      }
+    ],
+    activityDayMapping: "activityEnd",
+    phases: [
+      {
+        id: "single",
+        name: "Autumn 2024",
+        metrics: [
+          {
+            id: "pts",
+            frequency: "cumulative", // from the beginning of phase
+            inputs: [
+              {
+                id: "rideMileage",
+                source: "API", dataProvider: "Strava", sportGroups: ["Ride",],
+              },
+              {
+                id: "runMileage",
+                source: "API", dataProvider: "Strava", sportGroups: ["Run",],
+              },
+              {
+                id: "walkMileage",
+                source: "API", dataProvider: "Strava", sportGroups: ["Walk",],
+              },
+              {
+                id: "swimMileage",
+                source: "API", dataProvider: "Strava", sportGroups: ["Swim",],
+              },
+            ],
+            outputs: [
+              {
+                id: "ridePts",
+                computationRules: [{op: "mult", params: [{source: "inputs", id: "rideMileage",}, 0.25,],}, {
+                  op: "round",
+                  params: 2
+                },],
+              },
+              {
+                id: "runPts",
+                computationRules: [{op: "mult", params: [{source: "inputs", id: "runMileage",}, 0.25,],}, {
+                  op: "round",
+                  params: 2
+                },],
+              },
+              {
+                id: "walkPts",
+                computationRules: [{op: "mult", params: [{source: "inputs", id: "walkMileage",}, 0.25,],}, {
+                  op: "round",
+                  params: 2
+                },],
+              },
+              {
+                id: "swimPts",
+                computationRules: [{op: "mult", params: [{source: "inputs", id: "swimMileage",}, 0.25,],}, {
+                  op: "round",
+                  params: 2
+                },],
+              },
+              {
+                id: "pts",
+                computationRules: [
+                  {
+                    op: "sum",
+                    params: [{source: "outputs", id: ["rideMileage", "runMileage", "walkMileage", "swimMileage",]},],
+                  },
+                  {op: "round", params: 1},
+                ],
+              },
+            ],
+          },
+        ],
+      }
+    ],
+  },
+]
