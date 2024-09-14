bdc84106327ee6910ae4e8a349bb96d8e4960817
OleksiyRudenko <8686631+OleksiyRudenko@users.noreply.github.com>
2024-09-14 12:40:52 +0200

    dev env: support nvm4w

--- nvm4w.cmd
+++ nvm4w.cmd
@@ -1,4 +1,9 @@
-goto code
+@echo off
+setlocal
+
+set "beginComment=goto :endComment"
+
+%beginComment%
 > nvm current
 v20.17.0
 > nvm list
@@ -8,4 +13,4 @@ v20.17.0
     11.15.0
 > if current doesn't match package.json/engines.node expression then find the matching version (e.g. the biggest if node: ">=x.x.x") and use it
 > if previous fails then nvm install & nvm use package.json/engines.node value (stripped of conditions)
-:code
+:endComment
