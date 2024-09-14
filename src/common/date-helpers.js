a702f7a65be45e32a34ba664b0b817d0d23f841d
OleksiyRudenko <8686631+OleksiyRudenko@users.noreply.github.com>
2024-09-14 12:33:24 +0200

    common: date helpers

--- src/common/date-helpers.js
+++ src/common/date-helpers.js
@@ -4,6 +4,40 @@
  * @returns {string}
  */
 export const date2UTCString = date => {
-  if (typeof(date) === "string") date = new Date(date)
+  if (typeof (date) === "string") date = new Date(date)
   return date.toISOString()
 }
+
+/**
+ * Creates date in given TimeZone.
+ * If any of year, month, day is omitted then machine time is used for values.
+ * If any of hour, minute, second is omitted then zero is applied.
+ * @example dateWithTimeZone("Europe/Warsaw", 2024, 8, 1) - time in Warsaw, beginning of August 1, 2024
+ * @example dateWithTimeZone("Europe/Warsaw") - time in Warsaw at the today's day beginning where machine is located
+ * @example dateWithTimeZone() - time in London at the machine's day beginning, i.e. 23:00 of previous day if machine is in CET (Berlin, Warsaw...)
+ * Inspired by https://stackoverflow.com/questions/15141762/how-to-initialize-a-javascript-date-to-a-particular-time-zone
+ * @param timeZone - one of Intl.supportedValuesOf("timeZone")
+ * @param year
+ * @param month - 1..12, not monthIndex
+ * @param day
+ * @param hour
+ * @param minute
+ * @param second
+ * @returns {Date}
+ */
+export const dateWithTimeZone = (timeZone = "Europe/London", year, month, day, hour, minute, second) => {
+  const machineDate = new Date()
+  const monthIndex = typeof (month) === 'undefined' ? machineDate.getMonth() : --month
+  let date = new Date(
+    Date.UTC(year ?? machineDate.getFullYear(), monthIndex, day ?? machineDate.getDate(),
+      hour ?? 0, minute ?? 0, second ?? 0)
+  )
+
+  const utcDate = new Date(date.toLocaleString('en-US', {timeZone: "UTC"}))
+  const tzDate = new Date(date.toLocaleString('en-US', {timeZone: timeZone}))
+  const offset = utcDate.getTime() - tzDate.getTime()
+
+  date.setTime(date.getTime() + offset)
+
+  return date
+};
--- src/common/date-helpers.test.js
+++ src/common/date-helpers.test.js
@@ -1,17 +1,49 @@
-import {date2UTCString} from "./date-helpers.js"
+import {date2UTCString, dateWithTimeZone} from "./date-helpers.js"
+
+// For Jest to run ESM, in JetBrains IDE: Run > Edit configurations > Node options = --experimental-vm-modules
 
 // ensure test works in any timezone the running machine's time set in
 // https://stackoverflow.com/questions/15141762/how-to-initialize-a-javascript-date-to-a-particular-time-zone
 
-const inputs = [
-  new Date("September 13, 2024 3:20:34 pm"),
-  new Date(2024, 8, 13, 15, 20, 34),
-  "2024-09-13T15:20:34+02:00",
-  "2024-09-13T13:20:34Z",
-]
-
 const expectedUTCString = "2024-09-13T13:20:34.000Z"
 
-test('date to ISO UTC string', () => {
-  inputs.forEach(input => expect(date2UTCString(input)).toBe(expectedUTCString))
+const date2UTCStringTestCases = [
+  [new Date("September 13, 2024 3:20:34 pm"), expectedUTCString,],
+  [new Date(2024, 8, 13, 15, 20, 34), expectedUTCString],
+  ["2024-09-13T15:20:34+02:00", expectedUTCString],
+  ["2024-09-13T13:20:34Z", expectedUTCString],
+]
+
+const localMachineTime = new Date()
+const localMachineTimePreviousDay = new Date()
+localMachineTimePreviousDay.setDate(localMachineTime.getDate() - 1)
+const localMachineDatePreviousDayString = `${localMachineTimePreviousDay.getFullYear()}-${(localMachineTimePreviousDay.getMonth() + 1).toString().padStart(2, '0')}-${localMachineTimePreviousDay.getDate().toString().padStart(2, '0')}`
+
+const dateWithTimeZoneTestCases = [
+  [["Europe/Warsaw", 2024, 9, 14, 9, 52, 0],
+    "2024-09-14T07:52:00.000Z"],
+  [["Europe/Warsaw"],
+    `${localMachineDatePreviousDayString}T22:00:00.000Z`],
+  [["Europe/London"],
+    `${localMachineDatePreviousDayString}T23:00:00.000Z`],
+  [[],
+    `${localMachineDatePreviousDayString}T23:00:00.000Z`],
+]
+
+describe('date2UTCString: Positive scenarios', () => {
+  test.each(date2UTCStringTestCases)(
+    'converts %p into ISO UTC string %p',
+    (input, expectedUTCString) => {
+      expect(date2UTCString(input)).toEqual(expectedUTCString)
+    }
+  )
+})
+
+describe('dateWithTimeZone: Positive scenarios', () => {
+  test.each(dateWithTimeZoneTestCases)(
+    'creates date from %p. UTC is expected to be %p',
+    (input, expectedUTCString) => {
+      expect(dateWithTimeZone(...input).toISOString()).toEqual(expectedUTCString)
+    }
+  )
 })
