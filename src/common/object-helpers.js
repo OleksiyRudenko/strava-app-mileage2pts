61f576fc610a8b87a01cb391367f676d905cacbb
OleksiyRudenko <8686631+OleksiyRudenko@users.noreply.github.com>
2024-09-15 16:50:56 +0200

    common: object and array of objects squashing helpers

--- src/common/object-helpers.js
+++ src/common/object-helpers.js
@@ -1,12 +1,51 @@
+import {isNumber} from "./type-helpers.js";
+
 /**
  * Merges two objects.
  * Warning! Only objects that compatible with JSON methods are supported.
  * @param {Object} target object
  * @param {Object} source object
- * @param {Object|String} propsRules - properties handling rules. Default behaviour is overwriting.
+ * @param {Object|String} propRules - properties handling rules. Default behaviour is overwriting.
+ * @returns {Object} squashed object
  */
-export const squashObjects = (target, source, propsRules = 'overwrite') => {
+export const squashObjects = (target, source, propRules = 'overwrite') => {
   target = JSON.parse(JSON.stringify(target))
+
+  // squash in source properties using propRules
+  const targetProps = Object.keys(target)
+  const sourceProps = Object.keys(source)
+  sourceProps.forEach(prop => {
+    if (targetProps.some(tProp => tProp === prop)) {
+      // squash property using propRules
+      const rule = typeof propRules === 'string' || propRules[prop] === undefined
+        ? 'overwrite'
+        : propRules[prop]
+
+      switch (rule) {
+        case 'overwrite':
+          target[prop] = source[prop]
+          break
+        case 'sum': // distinguish between scalars and arrays; throw on Object
+          if (Array.isArray(target[prop]) || Array.isArray(source[prop])) {
+            target[prop] = [...target[prop], ...source[prop]]
+          } else {
+            target[prop] = (target[prop] ?? (isNumber(source[prop]) ? 0 : '')) + source[prop]
+          }
+          break
+        case 'append': // distinguish between scalars (numbers are treated as strings) and arrays; throw on Object;
+          if (Array.isArray(target[prop]) || Array.isArray(source[prop])) {
+            target[prop] = [...target[prop], ...source[prop]]
+          } else {
+            target[prop] = (target[prop] ?? '') + ('' + source[prop])
+          }
+          break
+        default: // function
+          target[prop] = rule(target[prop], source[prop])
+      }
+    } else {
+      target[prop] = source[prop]
+    }
+  })
 
   return target
 }
@@ -17,17 +56,51 @@ export const squashObjects = (target, source, propsRules = 'overwrite') => {
  * @param {Array} target array
  * @param {Array} source array
  * @param {String} idProp - property that identifies mergeable elements by its value equality
- * @param {Object|String} propsRules - properties handling rules. Default behaviour is overwriting.
+ * @param {Object|String} propRules - properties handling rules. Default behaviour is overwriting.
+ * @returns {Array} squashed arrays
  */
-export const squashArrays = (target, source, idProp = 'id', propsRules= 'overwrite') => {
+export const squashArrays = (target, source, idProp = 'id', propRules = 'overwrite') => {
   target = JSON.parse(JSON.stringify(target))
   return source.reduce((targetArray, sourceElement) => {
     const targetElement = targetArray.find(targetElement => targetElement[idProp] === sourceElement[idProp])
     if (targetElement === undefined) {
-      targetArray.append(sourceElement)
+      targetArray.push(sourceElement)
     } else {
       // squash in source properties using propRules
+      const targetProps = Object.keys(targetElement)
+      const sourceProps = Object.keys(sourceElement)
+      sourceProps.forEach(prop => {
+        if (targetProps.some(tProp => tProp === prop)) {
+          // squash property using propRules
+          const rule = typeof propRules === 'string' || propRules[prop] === undefined
+            ? 'overwrite'
+            : propRules[prop]
 
+          switch (rule) {
+            case 'overwrite':
+              targetElement[prop] = sourceElement[prop]
+              break
+            case 'sum': // distinguish between scalars and arrays; throw on Object
+              if (Array.isArray(targetElement[prop]) || Array.isArray(sourceElement[prop])) {
+                targetElement[prop] = [...targetElement[prop], ...sourceElement[prop]]
+              } else {
+                targetElement[prop] = (targetElement[prop] ?? (isNumber(sourceElement[prop]) ? 0 : '')) + sourceElement[prop]
+              }
+              break
+            case 'append': // distinguish between scalars (numbers are treated as strings) and arrays; throw on Object;
+              if (Array.isArray(targetElement[prop]) || Array.isArray(sourceElement[prop])) {
+                targetElement[prop] = [...targetElement[prop], ...sourceElement[prop]]
+              } else {
+                targetElement[prop] = (targetElement[prop] ?? '') + ('' + sourceElement[prop])
+              }
+              break
+            default: // function
+              targetElement[prop] = rule(targetElement[prop], sourceElement[prop])
+          }
+        } else {
+          targetElement[prop] = sourceElement[prop]
+        }
+      })
     }
     return targetArray
   }, target)
--- src/common/object-helpers.test.js
+++ src/common/object-helpers.test.js
@@ -1,3 +1,5 @@
+import {squashArrays, squashObjects} from "./object-helpers.js"
+
 const squashObjectsTestCases = [
   [
     {
@@ -6,7 +8,8 @@ const squashObjectsTestCases = [
         str2: 'def',
         arr1: ['a'],
         arr2: ['AA'],
-        arr4: [{id: 'i1', v: 'xyzi1'}, {id: 'i2', v: 'XYZi2'},],
+        arr4: [{id: 'i1', v: 'xyzi1'}, {id: 'i2', v: 'xyzi2'},],
+        arr5: [{id: 'i1', v: 'xyzi1'}, {id: 'i2', v: 'xyzi2'},],
       },
       source: {
         str1: 'ABC',
@@ -15,14 +18,13 @@ const squashObjectsTestCases = [
         arr2: ['BB'],
         arr3: ['CCC'],
         arr4: [{id: 'i1', v: 'XYZi1'}, {id: 'i3', v: 'XYZi3'},],
+        arr5: [{id: 'i1', v: 'XYZi1'}, {id: 'i3', v: 'XYZi3'},],
       },
       propRules: {
         arr2: 'append',
         arr3: 'append',
-        arr4: function (targetArray, sourceArray) {
-          // add/overwite strategy
-
-        }
+        arr4: (targetArray, sourceArray) => [...targetArray, ...sourceArray],
+        arr5: (targetArray, sourceArray) => squashArrays(targetArray, sourceArray, 'id'),
       }
     },
     {
@@ -32,31 +34,65 @@ const squashObjectsTestCases = [
       arr1: ['A'],
       arr2: ['AA', 'BB'],
       arr3: ['CCC'],
-      arr4: [{id: 'i1', v: 'XYZi1'}, {id: 'i2', v: 'XYZi2'}, {id: 'i3', v: 'XYZi3'},],
+      arr4: [{id: 'i1', v: 'xyzi1'}, {id: 'i2', v: 'xyzi2'}, {id: 'i1', v: 'XYZi1'}, {id: 'i3', v: 'XYZi3'},],
+      arr5: [{id: 'i1', v: 'XYZi1'}, {id: 'i2', v: 'xyzi2'}, {id: 'i3', v: 'XYZi3'},],
     }
   ],
 ]
 
-const squashArrayTestCases = [
+const squashArraysTestCases = [
   [
     {
       target: [
-        {id: 'i1', v1: 'xyzi1', v2: 'i1a'},
+        {id: 'i1', v1: 'xyzi1', v2: 'i1a', v3: 'i2v3a', v4: 5, v5: 7, xa1: ['xa1a'], xa2: ['xa2a']},
         {id: 'i2', v1: 'XYZi2', v2: 'i2a'},
       ],
       source: [
-        {id: 'i1', v1: 'XYZi1', v2: 'i1b'},
+        {id: 'i1', v1: 'XYZi1', v2: 'i1b', v3: 'i2v3b', v4: 6, v5: 9, xa1: ['xa1b'], xa2: ['xa2b'], xa3: ['xa3b'],},
         {id: 'i3', v1: 'XYZi3', v2: 'i3b'},
       ],
       idProp: 'id',
       propRules: {
-        v2: (target, source) => target + source,
+        v2: (target, source) => target + source, // equivalent to 'append' rule
+        v3: 'sum',
+        v4: 'sum',
+        v5: 'append',
+        xa2: 'append',
+        xa3: 'append',
       }
     },
     [
-      {id: 'i1', v1: 'XYZi1', v2: 'i1ai1b'},
+      {
+        id: 'i1',
+        v1: 'XYZi1',
+        v2: 'i1ai1b',
+        v3: 'i2v3ai2v3b',
+        v4: 11,
+        v5: '79',
+        xa1: ['xa1b'],
+        xa2: ['xa2a', 'xa2b'],
+        xa3: ['xa3b'],
+      },
       {id: 'i2', v1: 'XYZi2', v2: 'i2a'},
       {id: 'i3', v1: 'XYZi3', v2: 'i3b'},
     ],
   ],
 ]
+
+describe('squashArray: Positive scenarios', () => {
+  test.each(squashArraysTestCases)(
+    'squashes %p into array %p',
+    (input, expectedOutputArray) => {
+      expect(squashArrays(input.target, input.source, input.idProp, input.propRules)).toEqual(expectedOutputArray)
+    }
+  )
+})
+
+describe('squashObjects: Positive scenarios', () => {
+  test.each(squashObjectsTestCases)(
+    'squashes objects %p into object %p',
+    (input, expectedOutputObject) => {
+      expect(squashObjects(input.target, input.source, input.propRules)).toEqual(expectedOutputObject)
+    }
+  )
+})
